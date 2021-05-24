5b7d1cff840d5d385ed7d57da8b3eef9d5bd27ce
VaolEr <eremizin@gmail.com>
2021-05-24 20:00:56 +0300

    Init commit

---
+++ src/main/java/com/vaoler/assistantcsgobot/AssistantCsgoBotApplication.java
@@ -0,0 +1,13 @@
+package com.vaoler.assistantcsgobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistantCsgoBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssistantCsgoBotApplication.class, args);
+    }
+
+}
